a0f0e5da79f8f33546df5dfba39a3f47ee0fdfc3
Samuel Gondwe <samuelgondwe741@gmail.com>
2025-06-14 14:46:00 +0200

    implement error handler in server actions

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -27,10 +27,16 @@ export async function createInvoice(formData: FormData) {
   });
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
-  await sql`
+  // Todo: handling errors....
+  try {
+    await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+  } catch (error) {
+    console.log(error)
+  }
+ 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
@@ -44,18 +50,27 @@ export async function createInvoice(formData: FormData) {
     });
    
     const amountInCents = amount * 100;
-   
-    await sql`
+
+    // Todo: handling errors...
+    try {
+      await sql`
       UPDATE invoices
       SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
       WHERE id = ${id}
     `;
+    } catch (error) {
+      console.log(error)
+    }
+    
   
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
 
 export async function deleteInvoice(id: string) {
+  throw new Error("Failed to delete invoice")
+
+  // Unreachable code
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath("/dashboard/invoices");
 }
